d6490d027560649b6c6e1f6b4fb92585b3c3430c
winstantpay <ralf@winstantpay.com>
2018-05-30 22:43:38 +0700

    Cleanup comments

--- config.php
+++ config.php
@@ -1,8 +1,6 @@
 <?php
 return (object) array(
-    "username" => "Ralf4IOU",
-    "password" => "Letmein123@",
-    "callerId" => "773B3EBA-D4FC-4853-A32F-06FD23A5C902",
-    "FromWallet" => "RALF",
-	"ToWallet" => "HERVE"
+    "username" => "",
+    "password" => "",
+    "callerId" => ""
 );
